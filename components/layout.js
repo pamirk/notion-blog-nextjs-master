9f2cc1a90cec9c2b4053f7c68e0f4cfbeb79b74f
Pamir khan <pmysteryk@gmail.com>
2023-01-25 23:54:25 +0500

    add: adsbygoogle code

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,16 @@
-// components/layout.js
-
 import Header from './header'
 import Footer from './footer'
+import Head from "next/head";
 
 export default function Layout({children}) {
   return (
     <>
-      <Header titlePre="Home" />
+      <Head>
+        <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2484398083224760"
+                crossOrigin="anonymous"></script>
+      </Head>
+      <Header titlePre="Home"/>
       <main>{children}</main>
       <Footer/>
     </>
